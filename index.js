d0390590cc10f97e186ce49e8a3a0edcc773e3d2
f1lander <filanderuclez@gmail.com>
2022-12-13 23:40:21 -0600

    get Boolean input

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ async function run() {
     const name = core.getInput('name');
     const org = core.getInput('org');
     const accessToken = core.getInput('access-token');
-    const private = core.getInput('private');
+    const private = core.getBooleanInput('private');
 
     const endpoint = org ? `/orgs/${org}/repos` : '/user/repos'
     axios.post(
       'https://api.github.com' + endpoint,
       {
         name,
-        private: private === true,
+        private: !!private,
         auto_init: true
       },
       {
